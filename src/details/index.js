637943ab687e1dd3eb28dd14c3ac83f3d10a58a4
oshnigeria <142126825+oshnigeria@users.noreply.github.com>
2023-12-09 19:37:23 +0100

    Update index.js

--- src/details/index.js
+++ src/details/index.js
@@ -1,6 +1,6 @@
 export const main_url =
-  process.env.NODE_ENV === "production"
-    ? "https://api.oshnigeria.org"
-    : "https://sbxapi.oshnigeria.org";
+  process.env.NODE_ENV !== "production"
+    ? "https://sbxapi.oshnigeria.org"
+    : "https://api.oshnigeria.org";
 
 export const cookies_id = "oshnigeria_state_cookies";
