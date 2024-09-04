672a0ffd689c5627360e74bcc48d01799f445f74
chukwuyem99 <chukwuyem99@gmail.com>
2024-09-04 11:50:01 +0100

    corrections

--- src/details/index.js
+++ src/details/index.js
@@ -1,8 +1,8 @@
-// export const main_url =
-//   process.env.NEXT_PUBLIC_NODE_ENV !== "production"
-//     ? "https://sbxapi.oshnigeria.org"
-//     : "https://api.oshnigeria.org";
+export const main_url =
+  process.env.NEXT_PUBLIC_NODE_ENV !== "production"
+    ? "https://sbxapi.oshnigeria.org"
+    : "https://api.oshnigeria.org";
 
-export const main_url = "https://api.oshnigeria.org";
+// export const main_url = "https://api.oshnigeria.org";
 
 export const cookies_id = "oshnigeria_state_cookies";
